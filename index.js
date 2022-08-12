41fef541f345cced1ab23e3fbfa67ff941f932ff
Dakota Jones <dakota32jones@gmail.com>
2022-08-12 18:05:42 -0500

    broke up existing methods into modules

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-// TODO: Include packages needed for this application
-
-// TODO: Create an array of questions for user input
-const questions = [];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const { writeFile } = require("./utils/generateFile");
+const questions = require("./utils/questions");
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  console.log(questions);
+  writeFile();
+}
 
 // Function call to initialize app
 init();
---
+++ utils/generateFile.js
@@ -0,0 +1,11 @@
+// TODO: Create a function to write README file
+const fs = require("fs");
+
+function writeFile(path, content) {
+  // TODO: Create a file in the output folder
+  console.log("I did something");
+}
+
+module.exports = {
+  writeFile,
+};
